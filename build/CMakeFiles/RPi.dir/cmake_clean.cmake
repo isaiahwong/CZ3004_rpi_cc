72e02878cfabcda485ee96ae4eb218d962c1f498
isaiahwong <iisaiahwwong@gmail.com>
2022-01-22 16:27:09 +0800

    init

---
+++ build/CMakeFiles/RPi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPi.dir/main.cc.o"
+  "RPi"
+  "RPi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Adder/adder.cc
@@ -0,0 +1,3 @@
+#include "adder.h"
+
+float add(float a, float b) { return a + b; }
