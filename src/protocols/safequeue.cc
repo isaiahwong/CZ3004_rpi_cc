a082f64a21e94eb82ae6d88a1c5651cc15ee4ab1
isaiahwong <iisaiahwwong@gmail.com>
2022-01-23 22:54:50 +0800

    WIP PubSub for inter thread comms

---
+++ src/protocols/safequeue.cc
@@ -0,0 +1,52 @@
+/**
+ * @file safequeue.cc
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-23
+ *
+ * @copyright Copyright (c) 2022
+ * Adapted from
+ * https://stackoverflow.com/questions/15278343/c11-thread-safe-queue
+ */
+#ifndef SAFE_QUEUE
+#define SAFE_QUEUE
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+// A threadsafe-queue.
+template <class T>
+class SafeQueue {
+   public:
+    SafeQueue(void) : q(), m(), c() {}
+
+    ~SafeQueue(void) {}
+
+    // Add an element to the queue.
+    void enqueue(T t) {
+        std::lock_guard<std::mutex> lock(m);
+        q.push(t);
+        c.notify_one();
+    }
+
+    // Get the "front"-element.
+    // If the queue is empty, wait till a element is avaiable.
+    T dequeue(void) {
+        std::unique_lock<std::mutex> lock(m);
+        while (q.empty()) {
+            // release lock as long as the wait and reaquire it afterwards.
+            c.wait(lock);
+        }
+        T val = q.front();
+        q.pop();
+        return val;
+    }
+
+   private:
+    std::queue<T> q;
+    mutable std::mutex m;
+    std::condition_variable c;
+};
+#endif
