9ef47ff258201546c8b31f316d6ddce7ea82afc0
isaiahwong <iisaiahwwong@gmail.com>
2022-01-30 12:39:32 +0800

    Added grpc python

--- example/python/main.py
+++ example/python/main.py
@@ -1,7 +1,9 @@
-import time
+
 import grpc
-import cv2
 import numpy as np
+import cv2
+from model import Model
+
 from concurrent import futures
 
 from vision_pb2 import *
@@ -11,12 +13,16 @@ from vision_pb2_grpc import *
 class ImageServer(VisionServiceServicer):
     def __init__(self):
         self.count = 0
+        self.model = Model()
 
     def SendFrame(self, req, ctx):
         frame = np.array(list(req.image))
         frame = np.array(frame, dtype=np.uint8)
+        # Reshape pixels back to its dimensions
         array = np.reshape(frame, (req.height, req.width, req.channels))
-        # cv2.imwrite('out/frame{}.jpg'.format(self.count), array)
+
+        cv2.imwrite('out/frame{}.jpg'.format(self.count),
+                    self.model.predict(array, req.width, req.height, ))
         self.count += 1
         return Response(count=self.count)
 
